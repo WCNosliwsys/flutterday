979d9421450ec08461eb0f693056609d9dff6009
Wilson Callisaya <demarktec@gmail.com>
2019-11-23 13:12:43 -0500

    Añadiendo formato a los tickets

--- lib/ui/ticket/home/ticket_item.dart
+++ lib/ui/ticket/home/ticket_item.dart
@@ -6,12 +6,21 @@ class TicketItem extends StatelessWidget {
   TicketItem({this.title});
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(Icons.person),
-      title: Container(
-        height: 50.0,
-        width: 50.0,
-        child: Text(title), // si no existe un elemento se pone un valor por defecto
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      margin:EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.all(Radius.circular(10))
+      ),
+      child: ListTile(
+        leading: Icon(Icons.person),
+        title: Container(
+          height: 50.0,
+          width: 50.0,
+          child: Text(title), // si no existe un elemento se pone un valor por defecto
+        ),
       ),
     );
   }
